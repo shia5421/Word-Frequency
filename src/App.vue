cb85a7ff53d881ec93f5874eed2871f2acab2452
Rafael Filho <rafaelofil@outlook.com>
2020-06-14 14:25:06 -0300

    Array with words

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <BoxWord msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BoxWord from "./components/BoxWord.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    BoxWord
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/BoxWord.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="box_word">
+    <textarea
+      name="wordCounter"
+      id="wordCounter"
+      cols="30"
+      rows="10"
+      placeholder="Write here ;)"
+      v-model="text"
+      @keypress.space="wordSeparator()"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BoxWord",
+  data() {
+    return {
+      text: "",
+      words: [],
+      i: 0
+    };
+  },
+  methods: {
+    wordSeparator() {
+      this.words[this.i] = this.text.replace(/^(.*) /g, "");
+      this.i++;
+    }
+  }
+};
+</script>
+
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
